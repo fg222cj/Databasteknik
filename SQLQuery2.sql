65ac5e2643ed16ef14214cab99af903e21f9dba6
Fabian Gillholm <bakademoiihito@gmail.com>
2014-03-20 17:32:43 +0100

    Jobbat mer på labb 5

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -19,7 +19,7 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 
-/* Uppgift 2.a.1 */
+/* Uppgift 2a1 */
 
 CREATE PROCEDURE usp_InsertArtikel
 @Artnamn varchar(30),
@@ -34,8 +34,7 @@ BEGIN
 END
 GO
 
-
-/* Uppgift 2.a.2 */
+/* Uppgift 2a2 */
 
 CREATE PROCEDURE usp_UpdateArtikel
 @Artikelid int,
@@ -56,7 +55,7 @@ BEGIN
 END
 GO
 
-/* Uppgift 2.a.3 */
+/* Uppgift 2a3 */
 
 CREATE PROCEDURE usp_DeleteArtikel
 @Artikelid int = 0
@@ -66,3 +65,85 @@ BEGIN
 	WHERE ArtikelID = @Artikelid;
 END
 GO
+
+/* Uppgift 2b */
+
+ALTER PROCEDURE usp_SelectArtiklar
+@ArtikelID int = 0
+AS
+IF @ArtikelID = 0
+	BEGIN
+		SELECT ArtikelID, Artnamn, Antal, Pris, (Antal*Pris) AS Lagervärde
+		FROM Artikel;
+	END
+ELSE
+	BEGIN
+		SELECT ArtikelID, Artnamn, Antal, Pris, (Antal*Pris) AS Lagervärde
+		FROM Artikel
+		WHERE ArtikelID = @ArtikelID;
+		IF @@ROWCOUNT = 0
+			RAISERROR('Den specificerade artikeln existerar inte.', 16, 1)
+			RETURN (1)
+	END
+GO
+
+/* Uppgift 2c */
+
+ALTER PROCEDURE usp_Telefonlista
+@KundID int = 0
+AS
+IF @KundID = 0
+	BEGIN
+		WITH TelefonLista AS
+		(
+			SELECT Namn, Ort, Telenr, Teltyp
+			FROM Kund AS K LEFT JOIN Telefon AS T
+			ON K.KundID = T.KundID
+			LEFT JOIN Teltyp AS Tt
+			ON T.TeltypID = Tt.TelTypID
+		)
+		SELECT *
+		FROM TelefonLista
+		ORDER BY Namn, Teltyp;
+	END
+ELSE
+	BEGIN
+		WITH TelefonLista AS
+		(
+			SELECT Namn, Ort, Telenr, Teltyp
+			FROM Kund AS K LEFT JOIN Telefon AS T
+			ON K.KundID = T.KundID
+			INNER JOIN Teltyp AS Tt
+			ON T.TeltypID = Tt.TelTypID
+			WHERE K.KundID = @KundID
+		)
+		SELECT *
+		FROM TelefonLista
+		ORDER BY Namn, Teltyp;
+		IF @@ROWCOUNT = 0
+			RAISERROR('Den specificerade kunden existerar inte.', 16, 1)
+			RETURN (1)
+	END
+GO
+
+/* Uppgift 2d */
+
+CREATE PROCEDURE usp_ForsaljningsStatistik
+@FromDate date,
+@ToDate date
+AS
+BEGIN
+	SELECT A.ArtikelID, A.Artnamn, F.Datum, Fr.Antal, Fr.Pris, (A.Antal * A.Pris) AS Lagervärde
+	FROM Faktura AS F INNER JOIN Fakturarad AS Fr
+	ON F.FakturaID = Fr.FakturaID
+	INNER JOIN Artikel AS A
+	ON A.ArtikelID = Fr.ArtikelID
+	ORDER BY A.Artnamn;
+	IF @@ROWCOUNT = 0
+		RAISERROR('Det fanns inga fakturor inom det valda intervallet.', 16, 1)
+		RETURN (1)
+END
+GO
+
+/* Uppgift 3a */
+
